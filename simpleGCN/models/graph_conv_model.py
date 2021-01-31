6d5e2e1ae20814ab60dadc985690086d39085554
unknown <24367600+kimisyo@users.noreply.github.com>
2021-01-31 10:33:14 +0900

    change

--- simpleGCN/models/graph_conv_model.py
+++ simpleGCN/models/graph_conv_model.py
@@ -41,6 +41,15 @@ class GraphConvModel(pl.LightningModule):
             self.graph_pooling_layer_list.append(GraphPoolingLayer(layer_size, fingerprints_size))
             prev_layer_size = layer_size
 
+        for model in self.graph_conv_layer_list:
+            for param in model.parameters():
+                param.requires_grad = False;
+
+        for model in self.graph_pooling_layer_list:
+            for param in model.parameters():
+                param.requires_grad = False;
+
+
         prev_layer_size = fingerprints_size
         for i, layer_size in enumerate(mlp_layer_sizes):
             self.mlp_layer_list.append(torch.nn.Linear(prev_layer_size, layer_size, bias=True))
@@ -107,17 +116,30 @@ class GraphConvModel(pl.LightningModule):
             acc = accuracy(y_pred, labels)
             y_pred_proba = y_pred_proba.to('cpu').detach().numpy().tolist()
             from sklearn import metrics
-            rocauc = metrics.roc_auc_score(labels_list, y_pred_proba)
-
-        self.log('train_loss', loss, on_step=True, on_epoch=True)
+            try:
+                rocauc = metrics.roc_auc_score(labels_list, y_pred_proba)
+            except Exception as es:
+                rocauc = 0
 
         if self.task == "regression":
-            self.log('train_r2', r2, on_step=True, on_epoch=True)
+            ret = {'loss': loss, 'train_r2': r2}
         else:
-            self.log('train_acc', acc, on_step=True, on_epoch=True)
-            self.log('train_rocauc', rocauc, on_step=True, on_epoch=True)
+            ret = {'loss': loss, 'train_acc': acc, 'train_rocauc': torch.tensor(rocauc, dtype=torch.float)}
 
-        return loss
+        return ret
+
+    def training_epoch_end(self, outputs):
+        loss = torch.stack([x['loss'] for x in outputs]).mean()
+        self.log('train_loss', loss)
+
+        if self.task == "regression":
+            r2 = torch.stack([x['train_r2'] for x in outputs]).mean()
+            self.log('train_r2', r2)
+        else:
+            acc = torch.stack([x['train_acc'] for x in outputs]).mean()
+            self.log('train_acc', acc)
+            rocauc = torch.stack([x['train_rocauc'] for x in outputs]).mean()
+            self.log('train_rocauc', rocauc)
 
     def validation_step(self, batch, batch_idx):
 
@@ -155,62 +177,26 @@ class GraphConvModel(pl.LightningModule):
             except Exception as es:
                 rocauc = 0
 
-        self.log('val_loss', loss, on_step=True, on_epoch=True)
+        if self.task == "regression":
+            ret = {'val_loss': loss, 'val_r2': r2}
+        else:
+            ret = {'val_loss': loss, 'val_acc': acc, 'val_rocauc': torch.tensor(rocauc, dtype=torch.float)}
+
+        return ret
+
+    def validation_epoch_end(self, outputs):
+
+        loss = torch.stack([x['val_loss'] for x in outputs]).mean()
+        self.log('val_loss', loss)
 
         if self.task == "regression":
-            self.log('val_r2', r2, on_step=True, on_epoch=True)
+            r2 = torch.stack([x['val_r2'] for x in outputs]).mean()
+            self.log('val_r2', r2)
         else:
-            self.log('val_acc', acc, on_step=False, on_epoch=True)
-            self.log('val_rocauc', rocauc, on_step=False, on_epoch=True)
-
-        return loss
-
-
-    def test_step(self, batch, batch_idx):
-
-        array_rep, labels_list = batch
-        atom_features = array_rep['atom_features']
-        bond_features = array_rep['bond_features']
-
-        atom_features = torch.tensor(atom_features, dtype=torch.float)
-        bond_features = torch.tensor(bond_features, dtype=torch.float)
-        if self.method == "regression":
-            labels = torch.tensor(labels_list, dtype=torch.float)
-        else:
-            labels = torch.tensor(labels_list, dtype=torch.float)
-
-        atom_features = atom_features.to(self.device)
-        bond_features = bond_features.to(self.device)
-        labels = labels.to(self.device)
-
-        y_pred = self(array_rep, atom_features, bond_features)
-
-        if self.task == "regression":
-            loss = F.mse_loss(y_pred, labels)
-            # https://github.com/pytorch/ignite/issues/453
-            var_y = torch.var(labels, unbiased=False)
-            r2 = 1.0 - F.mse_loss(y_pred, labels, reduction="mean") / var_y
-        else:
-            loss = F.binary_cross_entropy_with_logits(y_pred, labels)
-            y_pred_proba = torch.sigmoid(y_pred)
-            y_pred = y_pred_proba > 0.5
-            acc = accuracy(y_pred, labels)
-            y_pred_proba = y_pred_proba.to('cpu').detach().numpy().tolist()
-            from sklearn import metrics
-            try:
-                rocauc = metrics.roc_auc_score(labels_list, y_pred_proba)
-            except Exception as es:
-                rocauc = 0
-
-        self.log('test_loss', loss, on_step=True, on_epoch=True)
-
-        if self.task == "regression":
-            self.log('test_r2', r2, on_step=True, on_epoch=True)
-        else:
-            self.log('test_acc', acc, on_step=True, on_epoch=True)
-            self.log('test_rocauc', rocauc, on_step=True, on_epoch=True)
-
-        return loss
+            acc = torch.stack([x['val_acc'] for x in outputs]).mean()
+            self.log('val_acc', acc)
+            rocauc = torch.stack([x['val_rocauc'] for x in outputs]).mean()
+            self.log('val_rocauc', rocauc)
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), self.lr)
